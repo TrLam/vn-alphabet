c11c5f07b891ffc1e7d68ec31136fcff48889485
Trang Lam <lam.diem.trang@gmail.com>
2022-02-13 17:28:34 -0500

    Updated layout and root font size

--- src/Alphabet/index.js
+++ src/Alphabet/index.js
@@ -8,12 +8,13 @@ const Alphabet = () => {
   return (
     <>
       <header className="p-3">Bảng chữ cái</header>
-      <div className="grid gap-5 grid-cols-5">
+      <div className="grid gap-5 grid-cols-3 sm:grid-cols-5">
         {ALPHABET.map((letterData) => (
           <Link to={`/${letterData.id}`} key={letterData.id}>
             <Letter letter={letterData.letter} />
           </Link>
         ))}
+        <div className="bg-transparent border border-blue-500 rounded" />
       </div>
     </>
   );
--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router';
 
 function App() {
   return (
-    <div className="font-serif bg-[#282c34] text-white text-6xl text-center min-h-screen flex flex-col items-center justify-center ">
+    <div className="font-mono bg-[#282c34] text-white text-6xl text-center min-h-screen flex flex-col items-center justify-center ">
       <Outlet />
     </div>
   );
